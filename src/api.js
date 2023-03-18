ea5d09fea6ff59b5fc43d10cd71bbcf6f40f32fd
Talhary <talhariaz5425869@gmail.com>
2023-03-18 16:15:19 +0000

    fdsf

--- src/api.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express')
-const router = require('./route.js')
-const app = express();
-const serverless = require('serverless-http')
-app.use('/.netlify/functions/api',router )
-
-
-module.exports.handler = serverless(app)
--- src/route.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router()
-router.get('/', (req, res)=>{
-    res.send({status: 200 , data: 'succesfull'})
-})
-module.exports = router
